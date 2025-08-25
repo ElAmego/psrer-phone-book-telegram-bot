1fbe28bf6b9b3bdba308ab125f9401689d8fd22d
Egor <el.amegoal@gmail.com>
2025-08-25 13:56:57 +0300

    refactoring | little refactoring

--- node/src/main/java/by/psrer/command/user/CommandStart.java
+++ node/src/main/java/by/psrer/command/user/CommandStart.java
@@ -5,6 +5,9 @@ import by.psrer.entity.AppUser;
 import by.psrer.utils.Answer;
 import by.psrer.utils.MessageUtils;
 import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+
+import java.util.List;
 
 @Service
 public final class CommandStart implements Command {
@@ -17,17 +20,16 @@ public final class CommandStart implements Command {
     @Override
     public void execute(final AppUser appUser) {
         final Long chatId = appUser.getTelegramUserId();
+        final List<InlineKeyboardButton> inlineKeyboardButtonList = messageUtils.createHelpCommand();
 
         final String startMessage = """
                 Добро пожаловать в телефонный справочник ПГРЭЗ.
-                
-                /help – список доступных команд.
                 
                 Контакты администратора для вопросов или обновления базы данных:
                 +37529*******
                 @admin_user
                 """;
 
-        messageUtils.sendTextMessage(chatId, new Answer(startMessage, null));
+        messageUtils.sendTextMessage(chatId, new Answer(startMessage, inlineKeyboardButtonList));
     }
 }
