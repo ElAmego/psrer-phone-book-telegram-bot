c7f5bac495d74970b06b5126fce64196edcfc8e2
Egor <el.amegoal@gmail.com>
2025-09-15 10:51:45 +0300

    update | updated help callback

--- node/src/main/java/by/psrer/callback/impl/CallbackHelp.java
+++ node/src/main/java/by/psrer/callback/impl/CallbackHelp.java
@@ -1,21 +1,29 @@
 package by.psrer.callback.impl;
 
 import by.psrer.callback.Callback;
-import by.psrer.command.impl.UserCommandContainerImpl;
 import by.psrer.entity.AppUser;
+import by.psrer.utils.Answer;
+import by.psrer.utils.MessageUtils;
 import org.springframework.stereotype.Service;
 
 @Service
 public final class CallbackHelp implements Callback {
-    private final UserCommandContainerImpl userCommandContainer;
+    private final MessageUtils messageUtils;
 
-    public CallbackHelp(final UserCommandContainerImpl userCommandContainer) {
-        this.userCommandContainer = userCommandContainer;
+    public CallbackHelp(MessageUtils messageUtils) {
+        this.messageUtils = messageUtils;
     }
 
     @Override
     public void execute(final AppUser appUser) {
-        userCommandContainer.retrieveCommand("/help")
-                .execute(appUser);
+        final String output = """
+                Список доступных команд:
+                /start – Стартовая страница бота.
+                /phones – Телефонная книга.
+                /search – Поиск номера по подстроке ФИО.
+                /help – Список доступных команд.
+                """;
+
+        messageUtils.sendReplacedTextMessage(appUser, new Answer(output, null));
     }
 }
